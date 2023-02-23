e449363037ffcdc6f8c3df547c715800853b40c4
nextitTester <Nextitcareer04@gmail.com>
2023-02-23 08:20:02 +0530

    Test

--- src/test/java/base/TestBase.java
+++ src/test/java/base/TestBase.java
@@ -123,6 +123,9 @@ public class TestBase {
 	
 	@BeforeTest(dependsOnMethods = "setUp")
 	public void excelFileSetup() throws IOException {
+		
+		
+		System.out.println(System.getenv("Browser"));
 		file=new File(System.getProperty("user.dir")+"\\src\\test\\resources\\excel\\testdata.xlsx");
 		fileInputStream=new FileInputStream(file);
 		workbook=new XSSFWorkbook(fileInputStream);
